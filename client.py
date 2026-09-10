6966576249d61b7a6d5ddbece1bc14308bfa6c73
uzo-presly <wort3rs@gmail.com>
2026-09-10 13:58:37 +0100

    modified client.py

--- client.py
+++ client.py
@@ -4,7 +4,7 @@ import sys
 
 from langchain_mcp_adapters.tools import load_mcp_tools
 from langchain_ollama import ChatOllama
-from langgraph.prebuilt import create_react_agent
+from langchain.agents import create_agent
 
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
@@ -33,7 +33,7 @@ async def run_agent(name_string):
         async with ClientSession(read, write) as session:
             await session.initialize()
             tools = await load_mcp_tools(session)
-            agent = create_react_agent(model, tools)
+            agent = create_agent(model, tools)
 
             prompt = get_prompt(name_string)
             agent_response = await agent.ainvoke({"messages": prompt})
